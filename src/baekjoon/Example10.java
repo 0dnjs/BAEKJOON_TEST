9817759cc469ff2fd6b7481ba2778804d0583467
0dnjs <non_ez@naver.com>
2023-07-03 17:51:52 +0900

    Feat: BAEKJOON_study 07/03

--- src/baekjoon/Example10.java
+++ src/baekjoon/Example10.java
@@ -25,10 +25,10 @@ public class Example10 {
 		int num1 = scanner.nextInt();
 		int num2 = scanner.nextInt();
 		
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
+		System.out.println(num1 * (num2 % 10));
+		System.out.println(num1 * ((num2 / 10 )% 10));
+		System.out.println(num1 * (num2 / 100));
+		System.out.println(num1 * num2);
 		
 	}
 
