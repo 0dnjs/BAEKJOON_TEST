e83547743cfc524f797220e9a1f4d87b0285ae5d
0dnjs <non_ez@naver.com>
2023-07-04 17:57:11 +0900

    Feat: BAEKJOON_STUDY 07/04

--- src/baekjoon/Example13.java
+++ src/baekjoon/Example13.java
@@ -13,6 +13,10 @@ public class Example13 {
 		 * | |_/=\\__|
 		 */
 
+		System.out.println("|\\_/|    /}");
+		System.out.println("|q p| \\");
+		System.out.println("( 0 )");
+		System.out.println("|q p|");
 	}
 
 }
